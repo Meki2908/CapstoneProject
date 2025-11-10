962fc7e5da421d11e8173af36f2f20e19b8318d4
nonghoangnam001 <hoangnam11jr@gmail.com>
2025-11-10 20:01:03 +0700

    fixloi

--- Assets/Terrains/PlayerActions.cs
+++ Assets/Terrains/PlayerActions.cs
@@ -72,7 +72,7 @@ using UnityEngine.InputSystem.Utilities;
 /// }
 /// </code>
 /// </example>
-public partial class @PlayerActions : IInputActionCollection2, IDisposable
+public partial class @PlayerActions_Actions: IInputActionCollection2, IDisposable
 {
     /// <summary>
     /// Provides access to the underlying asset instance.
@@ -82,7 +82,7 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     /// <summary>
     /// Constructs a new instance.
     /// </summary>
-    public @PlayerActions()
+    public @PlayerActions_Actions()
     {
         asset = InputActionAsset.FromJson(@"{
     ""version"": 1,
@@ -451,7 +451,7 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         m_Skill__4thSkill = m_Skill.FindAction("4th Skill", throwIfNotFound: true);
     }
 
-    ~@PlayerActions()
+    ~@PlayerActions_Actions()
     {
         UnityEngine.Debug.Assert(!m_Player.enabled, "This will cause a leak and performance issues, PlayerActions.Player.Disable() has not been called.");
         UnityEngine.Debug.Assert(!m_Skill.enabled, "This will cause a leak and performance issues, PlayerActions.Skill.Disable() has not been called.");
@@ -543,14 +543,14 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
-    public struct PlayerActionsMap
+    public struct PlayerActions
     {
-        private @PlayerActions m_Wrapper;
+        private @PlayerActions_Actions m_Wrapper;
 
         /// <summary>
         /// Construct a new instance of the input action map wrapper class.
         /// </summary>
-        public PlayerActionsMap(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+        public PlayerActions(@PlayerActions_Actions wrapper) { m_Wrapper = wrapper; }
         /// <summary>
         /// Provides access to the underlying input action "Player/Move".
         /// </summary>
@@ -602,9 +602,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.enabled" />
         public bool enabled => Get().enabled;
         /// <summary>
-        /// Implicitly converts an <see ref="PlayerActionsMap" /> to an <see ref="InputActionMap" /> instance.
+        /// Implicitly converts an <see ref="PlayerActions" /> to an <see ref="InputActionMap" /> instance.
         /// </summary>
-        public static implicit operator InputActionMap(PlayerActionsMap set) { return set.Get(); }
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
         /// <summary>
         /// Adds <see cref="InputAction.started"/>, <see cref="InputAction.performed"/> and <see cref="InputAction.canceled"/> callbacks provided via <param cref="instance" /> on all input actions contained in this map.
         /// </summary>
@@ -612,7 +612,7 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         /// <remarks>
         /// If <paramref name="instance" /> is <c>null</c> or <paramref name="instance"/> have already been added this method does nothing.
         /// </remarks>
-        /// <seealso cref="PlayerActionsMap" />
+        /// <seealso cref="PlayerActions" />
         public void AddCallbacks(IPlayerActions instance)
         {
             if (instance == null || m_Wrapper.m_PlayerActionsCallbackInterfaces.Contains(instance)) return;
@@ -655,7 +655,7 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         /// <remarks>
         /// Calling this method when <paramref name="instance" /> have not previously been registered has no side-effects.
         /// </remarks>
-        /// <seealso cref="PlayerActionsMap" />
+        /// <seealso cref="PlayerActions" />
         private void UnregisterCallbacks(IPlayerActions instance)
         {
             @Move.started -= instance.OnMove;
@@ -691,9 +691,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         }
 
         /// <summary>
-        /// Unregisters <param cref="instance" /> and unregisters all input action callbacks via <see cref="PlayerActionsMap.UnregisterCallbacks(IPlayerActions)" />.
+        /// Unregisters <param cref="instance" /> and unregisters all input action callbacks via <see cref="PlayerActions.UnregisterCallbacks(IPlayerActions)" />.
         /// </summary>
-        /// <seealso cref="PlayerActionsMap.UnregisterCallbacks(IPlayerActions)" />
+        /// <seealso cref="PlayerActions.UnregisterCallbacks(IPlayerActions)" />
         public void RemoveCallbacks(IPlayerActions instance)
         {
             if (m_Wrapper.m_PlayerActionsCallbackInterfaces.Remove(instance))
@@ -706,9 +706,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         /// <remarks>
         /// If <paramref name="instance" /> is <c>null</c>, calling this method will only unregister all existing callbacks but not register any new callbacks.
         /// </remarks>
-        /// <seealso cref="PlayerActionsMap.AddCallbacks(IPlayerActions)" />
-        /// <seealso cref="PlayerActionsMap.RemoveCallbacks(IPlayerActions)" />
-        /// <seealso cref="PlayerActionsMap.UnregisterCallbacks(IPlayerActions)" />
+        /// <seealso cref="PlayerActions.AddCallbacks(IPlayerActions)" />
+        /// <seealso cref="PlayerActions.RemoveCallbacks(IPlayerActions)" />
+        /// <seealso cref="PlayerActions.UnregisterCallbacks(IPlayerActions)" />
         public void SetCallbacks(IPlayerActions instance)
         {
             foreach (var item in m_Wrapper.m_PlayerActionsCallbackInterfaces)
@@ -718,9 +718,9 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
         }
     }
     /// <summary>
-    /// Provides a new <see cref="PlayerActionsMap" /> instance referencing this action map.
+    /// Provides a new <see cref="PlayerActions" /> instance referencing this action map.
     /// </summary>
-    public PlayerActionsMap @Player => new PlayerActionsMap(this);
+    public PlayerActions @Player => new PlayerActions(this);
 
     // Skill
     private readonly InputActionMap m_Skill;
@@ -734,12 +734,12 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     /// </summary>
     public struct SkillActions
     {
-        private @PlayerActions m_Wrapper;
+        private @PlayerActions_Actions m_Wrapper;
 
         /// <summary>
         /// Construct a new instance of the input action map wrapper class.
         /// </summary>
-        public SkillActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+        public SkillActions(@PlayerActions_Actions wrapper) { m_Wrapper = wrapper; }
         /// <summary>
         /// Provides access to the underlying input action "Skill/_1stSkill".
         /// </summary>
@@ -853,8 +853,8 @@ public partial class @PlayerActions : IInputActionCollection2, IDisposable
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "Player" which allows adding and removing callbacks.
     /// </summary>
-    /// <seealso cref="PlayerActionsMap.AddCallbacks(IPlayerActions)" />
-    /// <seealso cref="PlayerActionsMap.RemoveCallbacks(IPlayerActions)" />
+    /// <seealso cref="PlayerActions.AddCallbacks(IPlayerActions)" />
+    /// <seealso cref="PlayerActions.RemoveCallbacks(IPlayerActions)" />
     public interface IPlayerActions
     {
         /// <summary>
